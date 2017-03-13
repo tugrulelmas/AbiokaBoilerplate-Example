0cef74a244e32a10f43896144645235a4e99e719
tugrul <tugrulelmas@gmail.com>
2017-03-13 12:07:47 +0300

    added src

---
+++ AB.Host/src/app/shared/authentication/userService.js
@@ -0,0 +1,80 @@
+(function () {
+    'use strict';
+
+    angular.module('abioka')
+      .service('userService', userService);
+
+    /* @ngInject */
+    function userService($cookies) {
+        var user = getDefault();
+        var rememberMe = false;
+
+        var service = {
+            getUser: getUser,
+            setUser: setUser,
+            updateUser: updateUser,
+            destroy: destroy,
+            setRememberMe: setRememberMe
+        };
+
+        return service;
+
+        function setRememberMe(remember) {
+            rememberMe = remember;
+        }
+
+        function getUser() {
+            var userInfo = $cookies.getObject('userInfo', { path: '/' });
+            if (userInfo && userInfo.IsSignedIn === true) {
+                user = userInfo;
+            }
+            return user;
+        }
+
+        function setUser(token, callback) {
+            var isRememberMe = rememberMe || getUser().rememberMe;
+            var payload = Base64.decode(token.split('.')[1]);
+            var tokenUser = angular.fromJson(payload);
+
+            user.Id = tokenUser.id;
+            user.Email = tokenUser.email;
+            user.Provider = tokenUser.provider;
+            user.ExpirationDate = tokenUser.exp;
+            user.Token = token;
+            user.IsSignedIn = true;
+            user.RefreshToken = tokenUser.refresh_token;
+            user.Language = tokenUser.language;
+            if (isRememberMe) {
+                user.rememberMe = true;
+                setCookie(user);
+            }
+            callback(user);
+        }
+
+        function updateUser(userInfo) {
+            user.Language = userInfo.Language;
+            if (user.rememberMe) {
+                setCookie(user);
+            }
+        }
+
+        function destroy() {
+            var oldLanguage = user.Language;
+            user = getDefault();
+            user.Language = oldLanguage;
+            setCookie(user);
+        }
+
+        function getDefault() {
+            return {
+                Language: "en",
+                IsSignedIn: false
+            };
+        }
+
+        function setCookie(user) {
+            $cookies.remove('userInfo', { path: '/' });
+            $cookies.putObject('userInfo', user, { path: '/' });
+        }
+    }
+})();
